af6f69954dad879df9f2a589c06035a4d9474853
Vijay Kumar <saroyav3@gmail.com>
2024-05-26 11:35:04 +1200

    boo_details updated and added

---
+++ app/src/main/java/com/example/book_collection_manager/Book_Details.java
@@ -0,0 +1,14 @@
+package com.example.book_collection_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Book_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+    }
+}
